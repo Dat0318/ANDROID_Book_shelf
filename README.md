02efa754b10d04eae071594463bf7f65740436ae
Dat0318 <dattd@apecsoft.asia>
2021-02-17 15:14:53 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ANDROID_Book_shelf" 
---
+++ app/src/main/java/com/example/bookshelf/ui/home/HomeFragment.java
@@ -0,0 +1,55 @@
+package com.example.bookshelf.ui.home;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bookshelf.MainActivity;
+import com.example.bookshelf.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private static final String TAG = "HomeFragment";
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+      BottomNavigationView navView = root.findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+//        NavController navController = Navigation.findNavController(container.this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
+
+        return root;
+    }
+}
